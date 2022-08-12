f69abaf3b6be95e8dd13b8a875eb4a78b03d5953
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-12 17:14:20 +0200

    Change task status column

--- database/migrations/2022_08_11_161334_create_tasks_table.php
+++ database/migrations/2022_08_11_161334_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->unsignedBigInteger('todo_list_id');
-            $table->string('status')->default('not_completed');
+            $table->boolean('status')->default(false);
             $table->timestamps();
         });
     }
