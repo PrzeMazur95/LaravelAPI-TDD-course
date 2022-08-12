86db5dfbae28a71e2ba87317b31a74663a2122fd
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-12 17:38:37 +0200

    Deleted enums

--- database/migrations/2022_08_11_161334_create_tasks_table.php
+++ database/migrations/2022_08_11_161334_create_tasks_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use app\Enums\TaskEnums;
 
 return new class extends Migration
 {
@@ -17,7 +18,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->unsignedBigInteger('todo_list_id');
-            $table->string('status')->default("not_started");
+            $table->string('status')->default(TaskEnums::NOT_STARED->value);
             $table->timestamps();
         });
     }
