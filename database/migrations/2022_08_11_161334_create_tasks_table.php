dc7cb11c9dfdca1328e5c65ddd770e3a4b2bd653
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-18 22:05:10 +0200

    Add label_id to task migration

--- database/migrations/2022_08_11_161334_create_tasks_table.php
+++ database/migrations/2022_08_11_161334_create_tasks_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('title');
             $table->foreignId('todo_list_id')->constrained()->onDelete('cascade');
             $table->string('description')->nullable();
+            $table->foreignId('label_id')->constrained();
             $table->string('status')->default(Task::NOT_STARTED);
             $table->timestamps();
         });
