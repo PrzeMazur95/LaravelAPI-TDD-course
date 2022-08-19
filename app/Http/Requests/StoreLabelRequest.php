39f2780941e184a594badeac54e30c2e39adf267
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-19 22:19:24 +0200

    Fill store request validation

--- app/Http/Requests/StoreLabelRequest.php
+++ app/Http/Requests/StoreLabelRequest.php
@@ -13,7 +13,7 @@ class StoreLabelRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StoreLabelRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title'=>'required',
+            'color'=>'required'
         ];
     }
 }
