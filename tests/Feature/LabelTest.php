41b2e1ca4a85f1581fd0e4dcd902f19554b12239
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-19 22:24:58 +0200

    Add refreshdatabase to label migration to refresh bd before every test

--- tests/Feature/LabelTest.php
+++ tests/Feature/LabelTest.php
@@ -8,15 +8,12 @@ use Tests\TestCase;
 
 class LabelTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
+
     public function test_user_can_create_new_label()
     {
         $this->authUser();
-        
+
         $this->postJson(route('label.store', ['title'=>'my label', 'color' => 'red']))->assertCreated();
         $this->databaseHas('labels',['title'=>'my label', 'color' => 'red']);
     }
