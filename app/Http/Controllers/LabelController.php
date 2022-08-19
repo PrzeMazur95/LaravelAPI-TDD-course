23519747ac5f2fe963846f60b8ae81b9532c8688
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-19 22:20:12 +0200

    Import store label request to its controller

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Label;
+use App\Http\Requests\StoreLabelRequest;
 
 class LabelController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreLabelRequest $request)
     {
         //
     }
