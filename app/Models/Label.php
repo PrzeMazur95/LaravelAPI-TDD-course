50333098fe2ded4ecae0734843d240ccf413035d
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-19 22:48:55 +0200

    Fix typo

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -9,5 +9,5 @@ class Label extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'colot'];
+    protected $fillable = ['title', 'color'];
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\LabelController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,7 +27,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('todo-list.task', TaskController::class)
         ->except('show')
         ->shallow();
-        
+    Route::apiResource('label', LabelController::class);
 });
 
 
