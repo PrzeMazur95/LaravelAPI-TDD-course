9fad14e0faeb5d72a6c38f62a7c030bf49e58829
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-08-20 18:42:25 +0200

    Import Label model to Label test

--- tests/Feature/LabelTest.php
+++ tests/Feature/LabelTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Label;
 
 class LabelTest extends TestCase
 {
